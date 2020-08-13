631286fcd0de98c772bcfbac628202622bb405f8
shahabnoori <noorigoodarzi@gmail.com>
2020-08-13 17:53:25 +0430

    Initi Project

---
+++ Step-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Step_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+
+            Console.WriteLine("Please Enter First Name : ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Last Name :");
+            lastName = Console.ReadLine();
+
+
+            Console.WriteLine(firstName + " " + lastName);
+
+        }
+    }
+}
---
+++ Step-2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Step_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            const int arzeshAfzoode = 12;
+            //string numberOne;
+            //string numberTwo;
+            //Console.WriteLine("Entern Number One :: ");
+            //numberOne = Console.ReadLine();
+            //Console.WriteLine("Entern Number Two :: ");
+            //numberTwo = Console.ReadLine();
+            //var result = numberOne + numberTwo;
+
+            //int numberOne;
+            //int numberTwo;
+            //Console.WriteLine("Entern Number One :: ");
+            //numberOne = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Entern Number Two :: ");
+            //numberTwo = int.Parse(Console.ReadLine());
+            //var result = numberOne + numberTwo;
+            //ctrl+k+c => Comment
+            //ctrl+k+u => UnComment
+
+            //int myInt = 20;
+            //double myDoubleNumber = myInt; //Automatic Casting //Imlicitly
+            //myDoubleNumber = 12.6;
+            //myInt = (int)myDoubleNumber;
+            //arzeshAfzoode = 5;
+
+            int number;
+            number = int.Parse(Console.ReadLine());
+            var zojBoodan = number % 2;
+            var mazrabiAzPanjBoodan = number % 5;
+            var mazrabeChahar = number % 4;
+
+            //if (zojBoodan == 0)
+            //{
+            //    Console.WriteLine("Adad Zoj Ast");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Adad Fard Ast");
+            //}
+            //true && false =false
+            //    true && true=true
+
+            //if (zojBoodan == 0 && mazrabiAzPanjBoodan == 0)
+            //{
+            //    Console.WriteLine("Adad Zoj Mazrabe Panj Ast");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Adad Zoj Mazrabe Panj Nist");
+            //}
+
+
+            if (zojBoodan == 0 && (mazrabiAzPanjBoodan == 0 || mazrabeChahar==0))
+            {
+                Console.WriteLine("Adad Zoj Mazrabe Panj Ast");
+            }
+            else
+            {
+                Console.WriteLine("Adad Zoj Mazrabe Panj Nist");
+            }
+
+            //if (condition | Conditions)
+            //{
+            //    Statement
+            //}
+
+        }
+    }
+}
