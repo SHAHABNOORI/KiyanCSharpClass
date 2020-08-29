b251647804a38926579dd649cbabcb7a32e83e20
shahabnoori <noorigoodarzi@gmail.com>
2020-08-29 10:59:16 +0430

    Initial Step-3

--- KiyanCSharpClass/Step-3/Program.cs
+++ KiyanCSharpClass/Step-3/Program.cs
@@ -6,7 +6,102 @@ namespace Step_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int number = int.Parse(Console.ReadLine());
+            //if (number == 1)
+            //{
+            //    Console.WriteLine("One");
+            //}
+            //else if (number == 2)
+            //{
+            //    Console.WriteLine("Two");
+            //}
+            //else if (number == 3)
+            //{
+            //    Console.WriteLine("Three");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please enter number from 1 to 3");
+            //}
+
+            //switch (number)
+            //{
+            //    case 1:
+            //        Console.WriteLine("One");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Three");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Please enter number from 1 to 3");
+            //        break;
+            //}
+
+
+            //if (number == 1 || number == 2 || number == 3 || number == 5 || number == 7)
+            //{
+            //    Console.WriteLine("Adad Avale Ast");
+            //}
+            //else if (number % 2 != 0 && number % 3 != 0 && number % 5 != 0 && number % 7 != 0)
+            //{
+            //    Console.WriteLine("Adad Avale Ast");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Adad Aval Nist");
+            //}
+
+            //0 2 4 6 8
+
+
+            //int count = 0;
+            //for (int hasan = 100; hasan <= 998; hasan = hasan + 2)
+            //{
+            //    Console.WriteLine(hasan);
+            //    count = count + 1;
+            //}
+
+            //Console.WriteLine("-------------------------------");
+
+            //Console.WriteLine(count);
+
+            // (hade bala - hade pain) / game paresh + 1 
+            // (8 - 0)/2+1=5
+
+            //for (int i = 8; i >= 0; i = i - 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int hasan = 0; hasan <= 8; hasan = hasan + 2)
+            //{
+            //    Console.WriteLine(hasan);
+            //}
+
+            //int i = 0;
+            //while (i <= 8)
+            //{
+            //    Console.WriteLine(i);
+            //    i = i + 2;
+            //}
+
+
+            //int i = 8;
+            //while (i >= 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i = i - 2;
+            //}
+
+            int vorodi = int.Parse(Console.ReadLine());
+            while (vorodi!=0)
+            {
+                Console.WriteLine("*");
+                vorodi = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
