2970dde1019156634e1ea2d874d0322d7fcb0ae2
shahabnoori <noorigoodarzi@gmail.com>
2020-09-03 18:32:46 +0430

    Initial Method

---
+++ KiyanCSharpClass/SampleForMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SampleForMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //var c = Sum(a, b);
+            //Console.WriteLine(c);
+
+            var x = Sum(10, 30);
+            Hello();
+        }
+
+        static int Sum(int numberOne, int numberTwo)
+        {
+            var result = numberOne + numberTwo;
+            return result;
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("Hello My Dear");
+        }
+
+
+    }
+}
