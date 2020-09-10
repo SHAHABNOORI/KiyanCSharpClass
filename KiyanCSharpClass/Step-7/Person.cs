68a23519ac85eee1652a4b1b4f9166845d75fe2c
shahabnoori <noorigoodarzi@gmail.com>
2020-09-10 18:12:38 +0430

    Initial Step-7

---
+++ KiyanCSharpClass/Step-7/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Step_7
+{
+    public class Person
+    {
+        public Person()
+        {
+            _birthDate = 1340;
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            _age = age;
+        }
+
+        public string FirstName;
+
+        public string LastName;
+
+        private int _age = 100;
+
+        public void AgeSetter(int age)
+        {
+            if (age > 0 && age <= 120)
+            {
+                _age = age;
+            }
+            else
+            {
+                _age = 1;
+                Console.WriteLine("Your age is not correct");
+            }
+        }
+
+        //public int AgeGetter()
+        //{
+        //    return _age;
+        //}
+
+        // Expression Body 
+        public int AgeGetter() => _age;
+
+        private int _birthDate;
+
+        public int BirthDateYear
+        {
+            //get { return _birthDate; }
+            get => _birthDate;
+
+            set
+            {
+                if (value >= 1340)
+                {
+                    _birthDate = value;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter correct birth date year");
+                }
+            }
+        }
+
+        public void SayHello()
+        {
+            //Console.WriteLine("Hello Im " + FirstName + " " + LastName);
+            Console.WriteLine($"Hello Im {FirstName} {LastName} , {_age} years old , Birth Date Year = {BirthDateYear}");
+        }
+    }
+}
---
+++ KiyanCSharpClass/Step-7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Step_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shift + Alt + C  => Shortcut for creating class
+            // DRY => " Don't Repeat Yourself "
+            // Access Modifier سطح دسترسی
+            // Public 
+            // Private
+            // Property
+            // Constructor سازنده
+            // Parameterless constructor سازنده بدون پارامتر => untill you dont declare one
+            // Constructor => a Method with name of class , Without return type even void [ Initialize ]
+
+
+            //Person pOne = new Person()
+            //{
+            //    FirstName = "Shahab",
+            //    LastName = "Noori Goodarzi",
+            //    BirthDateYear = -1326
+            //};
+
+            //Person pOne = new Person("Shahab", "Noori Goodarzi")
+            //{
+            //    BirthDateYear = -1326
+            //};
+
+            Person pOne = new Person("Shahab", "Noori Goodarzi", 30)
+            {
+                BirthDateYear = -1326
+            };
+
+            //pOne.AgeSetter(-21);
+            //pOne.AgeSetter(21);
+            pOne.SayHello();
+            //Console.WriteLine($"Age is {pOne.AgeGetter()}");
+        }
+    }
+}
