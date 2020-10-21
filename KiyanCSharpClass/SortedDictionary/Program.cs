fe6e5c4f5d5378709246b3e76693864979f6855f
Shahab-Pc <Shahab-Pc@DESKTOP-3048NIP>
2020-10-21 20:19:21 +0330

    Init SortedList ANd Sorted Dictionary

---
+++ KiyanCSharpClass/SortedDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, string> students = new SortedDictionary<int, string>();
+            students.Add(10, "Shahab");
+            students.Add(30, "Alireza");
+            students.Add(20, "Hamid");
+            students.Add(40, "Mehdi");
+
+
+            foreach (var x in students)
+            {
+                Console.WriteLine($"{x.Key} {x.Value}");
+            }
+        }
+    }
+}
---
+++ KiyanCSharpClass/SortedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedListSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> students = new SortedList<int, string>();
+
+            students.Add(1, "Shahab");
+            students.Add(10, "Ali");
+            students.Add(2, "Mahdi");
+            students.Add(5, "Mina");
+            students.Add(3, "Vahid");
+
+
+            foreach (var a in students)
+            {
+                Console.WriteLine($"{a.Key} {a.Value}");
+            }
+        }
+    }
+}
