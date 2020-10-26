7f8c7ea2bf0039a57c8ca08905b6224b6626331a
Shahab-Pc <Shahab-Pc@DESKTOP-3048NIP>
2020-10-26 18:05:30 +0330

    Complete Generic Class

--- KiyanCSharpClass/GenericSample/GenericCustome.cs
+++ KiyanCSharpClass/GenericSample/GenericCustome.cs
@@ -2,13 +2,41 @@
 
 namespace GenericSample
 {
-    public class GenericCustome<T>
+    public class GenericCustome<T, TU, TV, TW>
     {
-        public T[] Vorodiha { get; set; }
+        public T[] VorodihaOne { get; set; }
 
-        public void Print()
+        public TU[] VorodihaTwo { get; set; }
+
+        public TV[] VorodihaThree { get; set; }
+
+        public TW MyVorodi { get; set; }
+
+
+        public void PrintOne()
         {
-            foreach (var vorodi in Vorodiha)
+            foreach (var vorodi in VorodihaOne)
+            {
+                Console.WriteLine(vorodi);
+            }
+        }
+
+        public void PrintTwo()
+        {
+            foreach (var vorodi in VorodihaOne)
+            {
+                Console.WriteLine(vorodi);
+            }
+        }
+
+        public TW PrintFour()
+        {
+            return MyVorodi;
+        }
+
+        public void PrintThree()
+        {
+            foreach (var vorodi in VorodihaOne)
             {
                 Console.WriteLine(vorodi);
             }
--- KiyanCSharpClass/GenericSample/Program.cs
+++ KiyanCSharpClass/GenericSample/Program.cs
@@ -14,11 +14,15 @@ namespace GenericSample
             //custom.Vorodiha = new[] { 10, 20, 30, 40, 50, 60 };
             //custom.Print();
 
-            GenericCustome<float> custom = new GenericCustome<float>
-            {
-                Vorodiha = new[] {10.5f, 20.5f, 30.10f, 40.20f, 50.20f, 60.6f}
-            };
-            custom.Print();
+            //GenericCustome<float> custom = new GenericCustome<float>
+            //{
+            //    Vorodiha = new[] {10.5f, 20.5f, 30.10f, 40.20f, 50.20f, 60.6f}
+            //};
+            //custom.Print();
+
+            GenericCustome<int, string, float, bool> custome = new GenericCustome<int, string, float, bool>();
+            custome.MyVorodi = true;
+            var result = custome.PrintFour();
         }
     }
 }
